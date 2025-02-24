9535b41c0a68881b8b087c40efa8fc1db62098be
juraevjavohirbek <javohirjorayev432@gmail.com>
2025-02-24 10:42:57 +0100

    third commit

--- src/Commits.java
+++ src/Commits.java
@@ -1,6 +1,5 @@
 public class Commits {
     public static void main(String[] args) {
-        System.out.println("Hello Vistula");
         System.out.println("It is my first project in java");
     }
 }
